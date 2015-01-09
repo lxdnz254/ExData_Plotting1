eac4d8427a32bc4b74d5f9abc2e7d022ad7fd31c
lxdnz254 <lxdnz34@gmail.com>
2015-01-10 11:16:59 +1300

    Update 2
    Added plot 4 and altered plot 3

--- plot4.R
+++ plot4.R
@@ -23,13 +23,19 @@ DT$DateTime <- as.POSIXct(paste(DT$Date,DT$Time), format="%d/%m/%Y %H:%M:%S")
 
 # Plot Data and save png file
 
-png(file ="plot3.png")
+png(file ="plot4.png")
+par(mfcol =c(2,2))
+plot(x=DT$DateTime, y=DT$Global_active_power, type = "l", xlab ="",
+     ylab = "Global Active Power")
 plot(range(DT$DateTime), range(c(DT$Sub_metering_1,
-                                DT$Sub_metering_2, DT$Sub_metering_3)), type='n',
-                                xlab = "", ylab ="Energy sub metering")
+                                 DT$Sub_metering_2, DT$Sub_metering_3)), type='n',
+     xlab = "", ylab ="Energy sub metering")
 lines(DT$DateTime, DT$Sub_metering_1, type='l') 
 lines(DT$DateTime, DT$Sub_metering_2, type='l', col='red')
 lines(DT$DateTime, DT$Sub_metering_3, type='l', col='blue')
-legend("topright", lty = 1, col = c("black", "red", "blue"), 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty = 1 , col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex =0.95, bty ='n')
+plot(DT$DateTime, y=DT$Voltage, type ="l", xlab="datetime", ylab ="Voltage")
+plot(DT$DateTime, y=DT$Global_reactive_power, type = "l", xlab = "datetime",
+     ylab = "Global_reative_power")
 dev.off()
